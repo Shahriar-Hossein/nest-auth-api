85a23370db280f7222c28d486082325c42b1453b
Shahriar-Hossein <shahriar2507@gmail.com>
2026-03-06 03:46:09 +0600

    created login register dto

---
+++ src/auth/dto/register.dto.ts
@@ -0,0 +1,29 @@
+import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class RegisterDto {
+	@IsString()
+	@MinLength(3)
+	@MaxLength(255)
+	firstName: string;
+
+	@IsString()
+	@MinLength(3)
+	@MaxLength(255)
+	lastName: string;
+
+	@IsString()
+	@MinLength(3)
+	@MaxLength(255)
+	username: string;
+	
+	@IsEmail()
+	@MaxLength(255)
+	email: string;
+
+	@IsString()
+	@MinLength(6)
+	@MaxLength(255)
+	password: string;
+
+	
+}
