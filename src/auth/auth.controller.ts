8373736ec8d80bd40ec46e2a273a20494411d06c
Shahriar-Hossein <shahriar2507@gmail.com>
2026-03-06 01:08:07 +0600

    updated auth controller for login and register route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service.js';
+
+type authInput = {
+  email: string;
+  password: string;
+};
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+	constructor(private readonly authService: AuthService) {}
+
+	@Post('register')
+	async register(@Body() input: authInput) {
+	}
+
+	@Post('login')
+	async login(@Body() input: authInput) {
+	}
+}
