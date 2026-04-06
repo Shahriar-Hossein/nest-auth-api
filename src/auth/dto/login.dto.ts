c989984e439e6db9e9805d169e0a0e20749adfb8
Shahriar-Hossein <shahriar2507@gmail.com>
2026-04-06 22:12:31 +0600

    formatted with prettier

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,13 +1,13 @@
 import { IsString, MaxLength, MinLength } from 'class-validator';
 
 export class LoginDto {
-	@IsString()
-	@MinLength(3)
-	@MaxLength(255)
-	identifier: string;
+  @IsString()
+  @MinLength(3)
+  @MaxLength(255)
+  identifier: string;
 
-	@IsString()
-	@MinLength(6)
-	@MaxLength(255)
-	password: string;
+  @IsString()
+  @MinLength(6)
+  @MaxLength(255)
+  password: string;
 }
--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,29 +1,27 @@
 import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class RegisterDto {
-	@IsString()
-	@MinLength(3)
-	@MaxLength(255)
-	firstName: string;
+  @IsString()
+  @MinLength(3)
+  @MaxLength(255)
+  firstName: string;
 
-	@IsString()
-	@MinLength(3)
-	@MaxLength(255)
-	lastName: string;
+  @IsString()
+  @MinLength(3)
+  @MaxLength(255)
+  lastName: string;
 
-	@IsString()
-	@MinLength(3)
-	@MaxLength(255)
-	username: string;
-	
-	@IsEmail()
-	@MaxLength(255)
-	email: string;
+  @IsString()
+  @MinLength(3)
+  @MaxLength(255)
+  username: string;
 
-	@IsString()
-	@MinLength(6)
-	@MaxLength(255)
-	password: string;
+  @IsEmail()
+  @MaxLength(255)
+  email: string;
 
-	
+  @IsString()
+  @MinLength(6)
+  @MaxLength(255)
+  password: string;
 }
